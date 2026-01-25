fa07c68ada72e33d63a5d88845a9ece9ff9a3169
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-01-25 20:22:44 +0100

    Model load test

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#include "./include/llama-cpp.h"
+
+#include <string>
+
+int main() {
+
+  std::string model_path = "C:\\Users\\Lasse\\Documents\\AI\\models\\gguf\\Qwen\\Qwen2.5-0.5B-Instruct-GGUF\\qwen2.5-0.5b-instruct-q8_0.gguf";
+
+  llama_model_params model_params = llama_model_default_params();
+  llama_model *model = llama_model_load_from_file(model_path.c_str(), model_params);
+
+
+} 
