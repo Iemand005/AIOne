da846fbc30e6988485ccdb02ae7b8adf574177a2
SmolFlexan <flexangaming@gmail.com>
2026-01-27 20:41:33 +0100

    Add batch files to compile standalone

--- AIOneQT/aioqt_exe.bat
+++ AIOneQT/aioqt_exe.bat
@@ -2,7 +2,7 @@
 cd build
 mkdir Deployed 2>nul
 cd Deployed
-copy ..\Desktop_Qt_6_10_1_MSVC2022_64bit-Debug\bin\AIOned.exe . 2>nul
+copy ..\BatCompiled\bin\Debug\AIOned.exe . 2>nul
 
 IF errorlevel 0 (
   C:\Qt\6.10.1\msvc2022_64\bin\windeployqt.exe --qmldir ..\.. AIOned.exe
