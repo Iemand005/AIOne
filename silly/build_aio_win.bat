5fa7af9665a682e0ad6d3fc1d0214075fef6024b
OFlexanWithout2FA <1467586@student.rocvantwente.nl>
2026-01-31 16:31:06 +0100

    Remove silly

--- silly/build_aio_win.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-
-mkdir bin 2>nul
-g++ -I..\include src\**.cpp -L.\lib -lllama -lggml -o bin\aione-cli.exe
-
-IF errorlevel 0 (
-  xcopy "lib\*.*" "bin\" /Y /I >nul
-)
--- silly/dev_aio_win.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call build_aio_win.bat
-
-IF errorlevel 0 (
-  cd bin
-  aione-cli.exe %*
-  cd ..
-)
