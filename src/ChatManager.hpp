9e25b9ab4d950b6f3bde6bd87863bfe80f0edb5c
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-01 12:30:36 +0100

    Change to chat manager

--- src/ChatManager.hpp
+++ src/ChatManager.hpp
@@ -2,15 +2,21 @@
 #include "ModelFactory.hpp"
 #include "Chat.hpp"
 
-class ModelManager {
+class ChatManager {
 
   std::unique_ptr<ModelFactory> factory = std::make_unique<ModelFactory>();
-  std::vector<Chat> chats;
+  std::vector<std::shared_ptr<Chat>> chats;
+  std::shared_ptr<Chat> currentChat;
+  LLModel *model;
 
   using OnToken = std::function<void(std::string token)>;
 
   void sendAsync(std::string message, OnToken callback) {
     
+  }
+
+  void setModel(LLModel *model) {
+    this->model = model;
   }
 
 };
