be012e355050b86dc661a08125ad7196257c7f6b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-05 23:18:51 +0100

    Add model manager

---
+++ src/ModelManager.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "ModelFactory.hpp"
+
+class ModelManager {
+
+  ModelFactoryPtr factory;
+
+  LLModelPtr llm;
+
+public:
+  ModelManager() {
+    factory = std::make_unique<ModelFactory>();
+  }
+
+  void loadLLMAsync(std::string path, LLModelOptions options) {
+    factory->loadLLMAsync(path, options, [this](LLModelPtr model) {
+      llm = std::move(model);
+    });
+  }
+};
