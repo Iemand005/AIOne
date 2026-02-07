701be59e3b8a5e4c1754406e465190fcb62af378
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-07 05:41:29 +0100

    Add wstring support

--- src/ModelManager.hpp
+++ src/ModelManager.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <string>
+#include <locale>
+#include <codecvt>
+
 #include "ModelFactory.hpp"
 #include "ChatManager.hpp"
 
@@ -13,6 +17,11 @@ class ModelManager {
 
 public:
   ModelManager() : factory(std::make_unique<ModelFactory>()) {}
+
+  void loadLLMAsync(std::wstring path, LLModelOptions options = {}) {
+      std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        loadLLMAsync(converter.to_bytes(path), options);
+  }
 
   void loadLLMAsync(std::string path, LLModelOptions options = {}) {
     factory->loadLLMAsync(path, options, [this](LLModelPtr model) {
